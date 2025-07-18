de4ca5b2defd3cf799e9b53c553bae61a2790c06
Nqadhafi <qadhafi817@gmail.com>
2025-07-18 15:50:24 +0700

    change name

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -6,7 +6,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Admin Panel</div>
+        <div class="sidebar-brand-text mx-3">Kasbon Shabat Printing</div>
     </a>
 
     <!-- Divider -->
